f32383b29de5079d1a92a9a2ad027550c68a45d3
Bekithemba Mdluli <bekithemba.mdluli.bm@gmail.com>
2020-12-01 12:45:50 +0200

    final commit

---
+++ lib/components/constants.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 80.0;
+const kActiveColor = Color(0xFF1D1E33);
+const kInactiveCardColour = Color(0xFF111328);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 10.0,
+  color: Color(0xFF8D8E98),
+);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 40.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kLargeButtonnTextStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultsTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
